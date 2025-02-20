ae31c35df9781d76cd27f9ae5277f84be3facdeb
osacr1216 <a111221062@mail.shu.edu.tw>
2025-02-20 11:51:27 +0800

    20250220

---
+++ 2025_02_20/Helloworld/Helloworld/Form1.cs
@@ -0,0 +1,25 @@
+namespace Helloworld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "¤£­n·í§L";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
