2f523ef017545a6cf9229f2ec2e68078a9e389a7
osacr1216 <a111221062@mail.shu.edu.tw>
2025-03-13 13:01:24 +0800

    2025_03_13

---
+++ 2025_03_13/Program5_9_1/Program5_9/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Program5_9
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random(); //創造亂數物件
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = rand.Next(1, 7); //產生1~6的亂數
+            int n2 = rand.Next(1, 7); //產生1~6的亂數
+
+            showPictureBox(n1,pictureBox1); //呼叫方法showpictureBox
+            showPictureBox(n2, pictureBox2); //呼叫方法showpictureBox2
+        }
+
+        private void showPictureBox(int n,PictureBox pic)
+        {
+            switch (n) //顯示第一個骰子的圖片
+            {
+                case 1:
+                    pic.Image = Properties.Resources._1Die;
+                    break;
+                case 2:
+                    pic.Image = Properties.Resources._2Die;
+                    break;
+                case 3:
+                    pic.Image = Properties.Resources._3Die;
+                    break;
+                case 4:
+                    pic.Image = Properties.Resources._4Die;
+                    break;
+                case 5:
+                    pic.Image = Properties.Resources._5Die;
+                    break;
+                case 6:
+                    pic.Image = Properties.Resources._6Die;
+                    break;
+            }
+        }
+    }
+}
+
---
+++ 2025_03_13/Program6_10/Program6_10/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Program6_10
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        string compChoice, myChoice, winner;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getCompChoice();
+        }
+
+        private void getCompChoice()
+        {
+            int n = rand.Next(1, 4);
+            switch(n)
+            {
+                case 1:
+                    compChoice = "石頭";
+                    break;
+                case 2:
+                    compChoice = "布";
+                    break;
+                case 3:
+                    compChoice = "剪刀";
+                    break;
+            }
+        }
+
+        private void showWinner()
+        {
+            if(myChoice == compChoice)
+                winner = "平手";            
+            else if (myChoice == "石頭" && compChoice == "剪刀")          
+                winner = "你贏了";           
+            else if (myChoice == "布" && compChoice == "石頭")           
+                winner = "你贏了";           
+            else if (myChoice == "剪刀" && compChoice == "布")  
+                winner = "你贏了";
+            else
+                winner = "電腦Wins";
+
+            label1.Text = "你選擇了" + myChoice + " 電腦選擇了" + compChoice + " " + winner;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myChoice = "石頭";
+            showWinner();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myChoice = "布";
+            showWinner();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            myChoice = "剪刀";
+            showWinner();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            getCompChoice();
+            label1.Text = "";
+        }
+
+
+    }
+}
